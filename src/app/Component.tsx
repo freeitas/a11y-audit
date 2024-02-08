ec6d73ce4eca91dd5388c4f1971232bdf30d2fa6
Guilherme Freitas <guilherme.f@onerhino.com>
2024-02-08 12:18:49 -0300

    errors

--- src/app/Component.tsx
+++ src/app/Component.tsx
@@ -3,16 +3,18 @@ import { useEffect, useState } from "react";
 import axe from "axe-core";
 
 export function Component(prop: any) {
-  const [errors, setErrors] = useState<any>([]);
+  const [errors, setErrors] = useState({});
 
   useEffect(() => {
     async function axep() {
       axe.run(prop, (err, results) => {
         console.log(results);
+        setErrors(errors => ({...errors, ...results}))
       });
     }
     axep();
+
   }, [prop]);
 
-  return <div></div>;
+  return <div>{errors ? JSON.stringify(errors.violations, null, 3) : 'b'}</div>;
 }
